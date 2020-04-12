7af1e1a75fd1a404907b7bb33196479d4aa105e4
KlementinaD <klementina_dimitrova@live.com>
2020-04-12 17:33:55 +0200

    student class finished

--- Student.java
+++ Student.java
@@ -2,16 +2,64 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
+	int []grades;
 
-	//TODO constructor
+	public Student(String index, String firstName, String lastName, int[] grades) {
+		this.index = index;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.grades = grades;
+	}
 
-	//TODO seters & getters
+	public String getIndex() {
+		return index;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public int[] getGrades() {
+		return grades;
+	}
+
+	public void setIndex(String index) {
+		this.index = index;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public void setGrades(int[] grades) {
+		this.grades = grades;
+	}
 
 	public double getAverage() {
-		//TODO
+		double avg;
+		int count;
+		for (int i=0;i,grades.length;i++){
+			avg+=grades[i];
+			count++;
+		}
+		return avg/count;
 	}
 
 	public int ECTSCredits() {
-		//TODO
+		int passed;
+		for (int i =0;i<grades.length;i++){
+			if (grades[i]>5){
+				passed++;
+			}
+		}
+		return passed*6;
 	}
 }
